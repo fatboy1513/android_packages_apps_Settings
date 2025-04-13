9519eed53f94cad323b0b6aec3032c0650b6be71
scp1513 <scp1513@gmail.com>
2025-04-13 23:28:27 +0800

    默认关闭"点按和点击提示音"
    Change-Id: I304e1276f7bcc6980d41e3060977d09582d1a49a

--- src/com/android/settings/notification/TouchSoundPreferenceController.java
+++ src/com/android/settings/notification/TouchSoundPreferenceController.java
@@ -35,7 +35,7 @@ public class TouchSoundPreferenceController extends SettingPrefController {
             Lifecycle lifecycle) {
         super(context, parent, lifecycle);
         mPreference = new SettingPref(
-            TYPE_SYSTEM, KEY_TOUCH_SOUNDS, System.SOUND_EFFECTS_ENABLED, DEFAULT_ON) {
+            TYPE_SYSTEM, KEY_TOUCH_SOUNDS, System.SOUND_EFFECTS_ENABLED, 0) {
             @Override
             protected boolean setSetting(final Context context, final int value) {
                 AsyncTask.execute(new Runnable() {
